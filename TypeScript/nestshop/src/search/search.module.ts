db29a44ca083de0fe904877c87d85062a1a767ea
hawon6691 <hawonoj@outlook.kr>
2026-02-23 10:11:56 +0900

    feat: (#113) ES 검색/자동완성/인덱싱 관리 API 구현

--- TypeScript/nestshop/src/search/search.module.ts
+++ TypeScript/nestshop/src/search/search.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PriceEntry } from '../price/entities/price-entry.entity';
 import { Product } from '../product/entities/product.entity';
@@ -10,7 +11,19 @@ import { SearchRecentKeyword } from './entities/search-recent-keyword.entity';
 import { SearchWeightSetting } from './entities/search-weight-setting.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, PriceEntry, User, SearchLog, SearchRecentKeyword, SearchWeightSetting])],
+  imports: [
+    TypeOrmModule.forFeature([Product, PriceEntry, User, SearchLog, SearchRecentKeyword, SearchWeightSetting]),
+    ElasticsearchModule.register({
+      node: process.env.ELASTICSEARCH_NODE ?? 'http://localhost:9200',
+      auth:
+        process.env.ELASTICSEARCH_USERNAME && process.env.ELASTICSEARCH_PASSWORD
+          ? {
+              username: process.env.ELASTICSEARCH_USERNAME,
+              password: process.env.ELASTICSEARCH_PASSWORD,
+            }
+          : undefined,
+    }),
+  ],
   controllers: [SearchController],
   providers: [SearchService],
   exports: [SearchService],
